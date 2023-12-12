3af65039f136672d8b2d0974e7f47bd854e808bd
MohammadMahdiGhasemi <ghasemi.ferdosi@gmail.com>
2023-12-12 23:36:31 +0330

    modified models ,add objects to DB

--- cafe/urls.py
+++ cafe/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from .views import home, product_detail, products, about
+from .views import home
 
 urlpatterns = [
     path("", home, name="home"),
-    path("breakfast/", products, name="breakfast"),
-    path("sweets/", products, name="sweets"),
-    path("cafe/", products, name="cafe"),
-    path("maincourse/", products, name="products"),
-    # path("products/<slug:slug>", products, name="products"),
-    # path("menu/", menu, name="menu"),
-    path("product_detail/<slug:slug>", product_detail, name="product_detail"),
-    path("about/", about, name="about"),
+    # path("breakfast/", products, name="breakfast"),
+    # path("sweets/", products, name="sweets"),
+    # path("cafe/", products, name="cafe"),
+    # path("maincourse/", products, name="products"),
+    # # path("products/<slug:slug>", products, name="products"),
+    # # path("menu/", menu, name="menu"),
+    # path("product_detail/<slug:slug>", product_detail, name="product_detail"),
+    # path("about/", about, name="about"),
 ]
--- orders/models.py
+++ orders/models.py
@@ -1,24 +1,28 @@
 from django.db import models
+from accounts.models import Staff,Customer
+
+class Bill (models.Model):
+   
+    total_cost=models.PositiveBigIntegerField()
+    description=models.CharField(max_length=500)
+    date_time=models.DateTimeField(auto_now_add=True)
 
 class Order(models.Model):
-    order_number=models.AutoField(primary_key=True)
     # item_id=models.ForeignKey(Item)
     description= models.CharField(max_length=500)
     date= models.DateField(auto_now_add=True)
     time= models.TimeField(auto_now_add=True)
     quantity=models.IntegerField()
+    staff=models.OneToOneField(to=Staff,to_field="id" , on_delete=models.DO_NOTHING)
+    bill=models.OneToOneField(to=Bill,to_field="id" ,on_delete=models.DO_NOTHING)
+    customer=models.OneToOneField(to=Customer , to_field="id" , on_delete=models.CASCADE)
 
 class Item(models.Model):
-    item_id=models.AutoField(primary_key=True)
-    order_id=models.ForeignKey(Order , on_delete= models.CASCADE )
     item_name=models.CharField(max_length=50)
     quantity=models.IntegerField()
     item_cost=models.IntegerField()
     description=models.CharField(max_length=500)
+    staff=models.ForeignKey(Staff , on_delete=models.DO_NOTHING)
+    order=models.ForeignKey(Order , on_delete= models.CASCADE )
 
-class Bill (models.Model):
-    bill_number=models.AutoField(primary_key=True)
-    total=models.IntegerField()
-    description=models.CharField(max_length=500)
-    date=models.DateField(auto_now_add=True)
-    time=models.TimeField(auto_now_add=True)
+
