faa6af0faaac0e6091cb0b554eb1ca7c58326310
MohammadMahdiGhasemi <ghasemi.ferdosi@gmail.com>
2023-12-18 21:49:29 +0330

    modify setting

--- core/settings.py
+++ core/settings.py
@@ -80,14 +80,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql', 
-        'NAME': env("DBNAME"),
-        'USER': env("DBUSER"),
-        'PASSWORD': env("DBPASSWORD"),
-        'HOST': env("DBHOST"),
-        'PORT': env("DBPORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql', 
+        # 'NAME': env("DBNAME"),
+        # 'USER': env("DBUSER"),
+        # 'PASSWORD': env("DBPASSWORD"),
+        # 'HOST': env("DBHOST"),
+        # 'PORT': env("DBPORT"),
         
     }
 }
