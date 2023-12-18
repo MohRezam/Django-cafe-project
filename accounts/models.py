a556767b5d0c193c7f272288dfad6b262b7d838c
MohRezam <mkalhor81126@gmail.com>
2023-12-19 01:42:17 +0330

    Add customer model from orders/models.py to accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 # from orders import models as orders_models
 role_list=(("Barista","Barista") ,('manager', 'Manager') , ("Cashier","Cashier"), ('waiter', 'Waiter'), ("Kitchen Staff","Kitchen Staff"))
 
 
 class Staff(models.Model):
-    # staff_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, null=True, blank=True)
     email = models.EmailField(max_length=255, null=True)
-    phone_number = models.IntegerField()
+    phone_number = models.CharField(max_length=11)
     role = models.CharField(max_length=255, choices=role_list)
+    
+    
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+
+
+# custom user
+class MyUser(AbstractBaseUser):
+    full_name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["email"]
+    
+    def __str__(self) -> str:
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin   
