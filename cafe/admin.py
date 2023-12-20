234de7be020baa338faa9ee61dd82b4e4d6ed87f
MohRezam <mkalhor81126@gmail.com>
2023-12-21 02:11:33 +0330

    Unregister Menu

--- cafe/admin.py
+++ cafe/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Menu, Category, Item
 # Register your models here.
 
-admin.site.register(Menu)
+# admin.site.register(Menu)
 admin.site.register(Category)
 admin.site.register(Item)
