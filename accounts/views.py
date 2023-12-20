6b26eaec11a6822497a826a306df8b171f4ce5c8
MohRezam <mkalhor81126@gmail.com>
2023-12-21 02:10:40 +0330

    Modified and add new views for cart page

--- accounts/views.py
+++ accounts/views.py
@@ -5,23 +5,6 @@ from .authenticate import PhoneBackend
 from django.contrib.auth import login
 # Create your views here.
 
-# def account(request):
-#     if request.method == "GET":
-#         return render(request, "", context={})
-    
-    
-# def bill(request):
-#     if request.method == "GET":
-#         return render(request, "", context={})
-    
-# def staff(request):
-#     if request.method == "GET":
-#         return render(request, "", context={})
-    
-# def manager(request):
-#     if request.method == "GET":
-#         return render(request, "", context={})
-    
     
 class UserLoginView(View):
     def get(self, request):
@@ -35,5 +18,5 @@ class UserLoginView(View):
             user = PhoneBackend.authenticate(request, username=cd["phone_number"], password=cd["password"])
             if user is not None:
                 login(request, user)
-                return render(request, "accounts/staff")
+                return render(request, "accounts/staff.html")
         return render(request, "accounts/login.html", {"form":form})
