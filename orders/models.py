c3eb0fd509ef60de032c07830507eef3d85ecc05
MohRezam <mkalhor81126@gmail.com>
2023-12-21 02:12:52 +0330

    Modified orders/models.py

--- orders/models.py
+++ orders/models.py
@@ -6,16 +6,17 @@ from django.contrib.sessions.models import Session
 
 
 class Order(models.Model):
-    # item_id=models.ForeignKey(Item)
     description= models.CharField(max_length=500)
     order_date = models.DateTimeField(auto_now_add=True ,null=True)
     table_number = models.IntegerField(null = True)
     customer = models.ForeignKey(Session, on_delete=models.CASCADE)
     
+    def __str__(self) -> str:
+        return f"{self.description[:20]}..."
 
 class OrderItem(models.Model):
-    # order_item_id = models.AutoField(primary_key=True)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.IntegerField()
+    
     
