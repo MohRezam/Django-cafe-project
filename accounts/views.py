c8daa54e422a75a978f239e046fa8e27372af47c
MohammadMahdiGhasemi <ghasemi.ferdosi@gmail.com>
2023-12-25 21:14:30 +0330

    modified account views , cafe template

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
+from django.http import Http404, HttpResponse
 from django.shortcuts import render, redirect
 from .forms import UserLoginForm
 from django.views import View
 from .authenticate import PhoneBackend
 from django.contrib.auth import login
+import datetime
+from  .models import User
 # Create your views here.
 
     
@@ -20,3 +23,23 @@ class UserLoginView(View):
                 login(request, user, backend="django.contrib.auth.backends.ModelBackend")
                 return render(request, "accounts/staff.html")
         return render(request, "accounts/login.html", {"form":form})
+
+
+
+def Usercookiesview(request, user_id):
+
+    user = user_id_func(user_id)
+    request.session['recent_event_id'] = user.id
+    request.session['recent_event_title'] = user.name
+
+    response = HttpResponse("Event View")
+    response.set_cookie('user_id', f"{user.id}")
+    response.set_cookie('user.name' , f"{user.name}")
+
+
+def user_id_func (user_id):
+    try:
+        event = User.objects.get(id=user_id)
+        return event
+    except User.DoesNotExist:
+        raise Http404("Event does not exist")
