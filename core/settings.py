d24818f18d57a0f6445b5fb12aa99c6caf017734
MohRezam <mkalhor81126@gmail.com>
2023-12-31 01:49:58 +0330

    Added MEDIA_ROOT & MEDIA_URL

--- core/settings.py
+++ core/settings.py
@@ -133,6 +133,11 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+# media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
