f3ab94d657a3fac719fc963cf8871c1b69636fbb
MohRezam <mkalhor81126@gmail.com>
2023-12-31 01:44:48 +0330

    Added new Cafe model

--- cafe/models.py
+++ cafe/models.py
@@ -1,35 +1,61 @@
 from django.db import models
 from accounts.models import TimeStampedModel
-
+from django.core.exceptions import ValidationError
 
 class Menu(models.Model):
     menu_name = models.CharField(max_length=255)
     description = models.TextField()
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=255, unique=True)
+    category_name = models.CharField(verbose_name="دسته بندی", max_length=255, unique=True)
+    image = models.ImageField(verbose_name="تصویر", upload_to="categories/%Y/%m/%d/")
     
     
     class Meta:  
-        verbose_name = 'Category'
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'دسته بندی محصولات'
     
     def __str__(self) -> str:
-        return f"Category: {self.category_name}"
+        return f"دسته بندی: {self.category_name}"
 
 class Item(TimeStampedModel):
-    name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
-    category = models.ForeignKey(Category, to_field="category_name", on_delete=models.CASCADE)
-    ingredients = models.TextField()
-    item_status = models.BooleanField(default=True)
+    name = models.CharField(verbose_name="محصول", max_length=255)
+    price = models.IntegerField(verbose_name="قیمت")
+    image = models.ImageField(verbose_name="تصویر", upload_to="items/%Y/%m/%d/")
+    description = models.TextField(verbose_name="توضیحات")
+    category = models.ForeignKey(Category, verbose_name="دسته بندی", to_field="category_name", on_delete=models.CASCADE)
+    ingredients = models.TextField(verbose_name="محتویات")
+    item_status = models.BooleanField(verbose_name="موجود", default=True)
+    
+    class Meta:  
+        verbose_name_plural = 'محصولات'
     
     def __str__(self) -> str:
-        return f"Item: {self.name}"
+        return f"محصول: {self.name}"
 class Table(models.Model):
-    table_number = models.IntegerField(unique=True)
-    is_available = models.BooleanField(default=True)
+    table_number = models.IntegerField(verbose_name="شماره میز", unique=True)
+    is_available = models.BooleanField(verbose_name="موجود", default=True)
 
     def __str__(self):
         return f"Table {self.table_number} - {'Available' if self.is_available else 'Not Available'}"
+
+
+class Cafe(models.Model):
+    name = models.CharField(verbose_name="نام", max_length=100)
+    logo_image = models.ImageField(verbose_name="تصویر لوگو", upload_to="cafe/%Y")
+    about_page_image = models.ImageField(verbose_name="تصویر صفحه توضیحات", upload_to="cafe/%Y")
+    about_page_description = models.TextField(verbose_name="توضیحات")
+    address = models.TextField(verbose_name="آدرس")
+    phone_number = models.CharField(verbose_name="شماره تماس", max_length=11)
+    
+    class Meta:
+        verbose_name_plural = 'کافه'
+        
+        
+    def save(self, *args, **kwargs):
+        if not self.pk and Cafe.objects.exists():
+            raise ValidationError('There is can be only one JuicerBaseSettings instance')
+        return super(Cafe, self).save(*args, **kwargs)
+        
+        
+    def __str__(self) -> str:
+        return self.name
