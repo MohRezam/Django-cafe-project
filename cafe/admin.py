b51189b8f690447413c3d30a294d45950030aa16
MohRezam <mkalhor81126@gmail.com>
2023-12-31 01:43:04 +0330

    Register new Cafe model

--- cafe/admin.py
+++ cafe/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Menu, Category, Item
+from .models import Menu, Category, Item, Cafe
 # Register your models here.
 
 # admin.site.register(Menu)
 admin.site.register(Category)
 admin.site.register(Item)
+admin.site.register(Cafe)
