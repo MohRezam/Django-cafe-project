64858bdee309feaf8f5b517e84eb7f045440b193
MohRezam <mkalhor81126@gmail.com>
2023-12-31 01:43:43 +0330

    Added SearchForm for searching in items

--- cafe/forms.py
+++ cafe/forms.py
@@ -4,3 +4,7 @@ from django import forms
 class CartAddForm(forms.Form):
     quantity = forms.CharField()
     iditem=forms.CharField()
+    
+
+class SearchForm(forms.Form):
+    search = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"جست و جو محصولات"}))
