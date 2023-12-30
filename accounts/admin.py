065255996c56aef16c456afbc8c210e509b34399
MohRezam <mkalhor81126@gmail.com>
2023-12-31 01:39:28 +0330

    Added address and national_id into the fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -14,12 +14,12 @@ class UserAdmin(BaseUserAdmin):
     list_filter = ('is_admin',)
     
     fieldsets = (
-        (None, {'fields': ('email', 'phone_number', 'full_name', 'password')}),
+        (None, {'fields': ('email', 'phone_number', 'full_name', 'password', 'address', 'national_id')}),
         ('Permissions', {'fields': ('is_active', 'is_admin','last_login')}),
     )
 
     add_fieldsets = (
-        (None, {'fields':('phone_number', 'email', 'full_name', 'password1', 'password2')}),
+        (None, {'fields':('phone_number', 'email', 'full_name', 'password1', 'password2', 'address', 'national_id')}),
     )
     search_fields = ('email', 'full_name')
     ordering = ('full_name',)
