73ce6c307ec05ad7bded40c3a8c5ce0bbf19f25f
MohRezam <mkalhor81126@gmail.com>
2023-12-31 01:51:32 +0330

    Added verbose_name_plural

--- orders/models.py
+++ orders/models.py
@@ -7,12 +7,14 @@ from accounts.models import TimeStampedModel
 
 
 class Order(TimeStampedModel):
-    description= models.CharField(max_length=500)
-    order_date = models.DateTimeField(auto_now_add=True ,null=True)
-    table_number = models.IntegerField(null = True)
+    description= models.CharField(verbose_name="توضیحات", max_length=500)
+    order_date = models.DateTimeField(verbose_name="زمان ثبت سفارش", auto_now_add=True ,null=True)
+    table_number = models.IntegerField(verbose_name="شماره میز", null = True)
     session = models.OneToOneField(Session, on_delete=models.CASCADE)
     # define a foriegn key to staff id and it should be unique
     
+    class Meta:
+        verbose_name_plural = 'سفارشات'
     def __str__(self) -> str:
         return f"{self.description[:20]}..."
 
