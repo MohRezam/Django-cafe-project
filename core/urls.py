2064133c2680c493ed01df688f3f86fd8dd4487a
MohRezam <mkalhor81126@gmail.com>
2023-12-31 01:50:36 +0330

    Added static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('cafe.urls', namespace="cafe")),
     path("accounts/", include("accounts.urls", namespace='accounts')),
     path("orders/", include('orders.urls', namespace="orders")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
