4340e4163e75d7c305cce4a07e69b267313e1e0a
MohammadMahdiGhasemi <ghasemi.ferdosi@gmail.com>
2024-01-04 02:10:51 +0330

    Added orderform

--- orders/forms.py
+++ orders/forms.py
@@ -1,6 +1,22 @@
 from django import forms
+from .models import Order
 
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['name', 'phone_number', 'table_number', 'discount_code']
 
 class UserSessionForm(forms.Form):
     phone_number=forms.CharField(max_length=11)
 
+
+
+# class CartEditForm(forms.Form):
+#     iditem = forms.IntegerField(widget=forms.HiddenInput())
+#     quantity = forms.IntegerField(min_value=1)
+
+#     def clean_quantity(self):
+#         quantity = self.cleaned_data['quantity']
+#         if quantity < 1:
+#             raise forms.ValidationError("Quantity should be greater than or equal to 1.")
+#         return quantity
