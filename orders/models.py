86b35028e0eb669b8d81f09cf6cfbcf0341e2b4b
MohammadMahdiGhasemi <ghasemi.ferdosi@gmail.com>
2024-01-03 23:14:01 +0330

    modified order model

--- orders/models.py
+++ orders/models.py
@@ -4,31 +4,38 @@ from cafe.models import Item
 # from accounts.models import User
 from django.contrib.sessions.models import Session
 from accounts.models import TimeStampedModel
+from accounts.models import User
+from django.contrib.postgres.fields import JSONField
 
 
 class Order(TimeStampedModel):
-    description= models.CharField(verbose_name="توضیحات", max_length=500)
+    description= models.CharField(verbose_name="توضیحات", max_length=500, null=True , blank=True)
     order_date = models.DateTimeField(verbose_name="زمان ثبت سفارش", auto_now_add=True ,null=True)
     table_number = models.IntegerField(verbose_name="شماره میز", null = True)
-    session = models.OneToOneField(Session, on_delete=models.CASCADE)
-    # define a foriegn key to staff id and it should be unique
-    # define a order_id (this is which we generate it in CafeMenuView )
-    
+    staff_id= models.ForeignKey(User , on_delete=models.CASCADE)
+    order_detail= models.JSONField(default=dict) #save like dictionary
+    customer_name= models.CharField(verbose_name="نام مشتری",blank = True , null = True , max_length=255) 
+    phone_number = models.CharField(verbose_name="شماره تلفن",blank = True , null = True , max_length=11) 
+    table_number= models.IntegerField(verbose_name=" شماره میز ")
+    disacount_code=models.CharField(verbose_name="کد تخفیف ",max_length=255)
+
+
     class Meta:
         verbose_name_plural = 'سفارشات'
     def __str__(self) -> str:
         return f"{self.description[:20]}..."
 
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
+# class OrderItem(models.Model):
+#     order = models.ForeignKey(Order, on_delete=models.CASCADE)
+#     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+#     quantity = models.IntegerField()
     
 
 
 
 
-# class Discount (models.Model):
-#     code = models.CharField()
-#     amount= models.IntegerField()
+class Discount (models.Model):
+    code = models.CharField()
+    amount= models.IntegerField()
+    expired_time= models.DateTimeField(auto_now_add=True)
 
