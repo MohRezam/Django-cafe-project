1aa87d1a8830f47ff15777f78237f8a4e0d6f7b0
MohammadMahdiGhasemi <ghasemi.ferdosi@gmail.com>
2024-01-04 02:47:25 +0330

    Added form for discount

--- orders/forms.py
+++ orders/forms.py
@@ -6,6 +6,9 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ['name', 'phone_number', 'table_number', 'discount_code']
 
+class DiscountCodeForm(forms.Form):
+    discount_code = forms.CharField(max_length=255)
+
 class UserSessionForm(forms.Form):
     phone_number=forms.CharField(max_length=11)
 
