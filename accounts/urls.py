e32b57fb05f88297dffa910e952e53332ad61bfe
ehsan <ehsan.1383asg63@yahoo.com>
2024-01-03 21:51:48 +0330

    Update views code

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,5 +11,11 @@ urlpatterns = [
     path('Logout/',views.StffLogoutView.as_view(),name="staff-logout"),
     path('profile-info/<int:staff_user_id>/',views.StaffProfileInfoView.as_view(),name="staff-profile-info"),
     path('profile-personal-info/',views.StaffProfilePersonalView.as_view(),name="staff-personal-info"),
-    path('profile-categories/',views.StaffProfileCategories.as_view(),name="staff-categories")
+    path('profile-categories/',views.StaffProfileCategoriesView.as_view(),name="staff-categories"),
+    path('profile-category-delete/<int:id_category>/',views.StaffCategoryDeleteView.as_view(),name="staff-category-delete"),
+    path('profile-category-update/<int:id_category>/',views.StaffCategoryUpdateView.as_view(),name="staff-category-update"),
+    path('profile-add-category/',views.StaffAddCategoryView.as_view(),name="staff-add-category"),
+    path('profile-items/',views.StaffProfileItemsView.as_view(),name="staff-items"),
+    path('profile-item-delete/<int:id_item>/',views.StaffProfileDeleteItemView.as_view(),name="staff-delete-item"),
+    path('profile-item-update/<int:id_item>/',views.StaffProfileUpdateView.as_view(),name="staff-update-item")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,15 @@
-from django.shortcuts import render, redirect
-from .forms import UserLoginForm,UserForm,UserChangeForm
+from django.shortcuts import render, redirect , get_object_or_404
+from .forms import UserLoginForm,UserForm,UserChangeForm,CategoryForm,ItemForm
 from django.views import View
 from cafe.models import Item,Category
 from accounts.models import User
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import CategoryForm
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login,authenticate,logout
-from .forms import AddItemForm
-from .forms import RemoveItemForm
-from .forms import EditItemStatusForm
 from django.views.generic import TemplateView
 from django.db.models import Count, Sum
 from django.utils import timezone
-from .models import OrderItem, Order
+from orders.models import OrderItem, Order
 import csv
 from django.http import HttpResponse
 # Create your views here.
@@ -70,75 +65,10 @@ class StffLogoutView(LoginRequiredMixin, View):
 		return redirect('accounts:staff-login')
 
 
-class AddCategoryView(View):
-    def get(self,request):
-        form = CategoryForm()
-        return render(request, 'profile-add-category.html', {'form': form})
-
-    def post(self,request):
-            form = CategoryForm(request.POST)
-            if form.is_valid():
-                category = Category(
-                    name=form.cleaned_data['name']
-                )
-                category.save()
-                messages.success(request, 'دسته بندی با موفقیت اضافه شد.')
-                return render(request, 'profile-add-category.html', {'form': CategoryForm()})
 
 
-class AddItemView(View):
-    def get(self,request):
-        form = AddItemForm()
-        return render(request, 'profile-item-add.html', {'form': form})
 
-    def post(self,request):
-        form = AddItemForm(request.POST, request.FILES)
-        if form.is_valid():
-            item = Item(
-                name=form.cleaned_data['name'],
-                fixed_number=form.cleaned_data['fixed_number'],
-                category=form.cleaned_data['category'],
-                description=form.cleaned_data['description'],
-                form_file=form.cleaned_data['form_file']
-            )
-            item.save()
-            messages.success(request, 'آیتم با موفقیت اضافه شد.')
-            return redirect('profile-items.html')
-
-
-class RemoveItemView(View):
-    def get(self, request):
-        form = RemoveItemForm()
-        return render(request, 'remove_item.html', {'form': form})
-
-    def post(self, request):
-        form = RemoveItemForm(request.POST)
-        if form.is_valid():
-            item_id = form.cleaned_data['item_id']
-            item = Item.objects.get(id=item_id)
-            item.delete()
-            messages.success(request, "محصول با موفقیت حذف شد") 
-            return redirect('remove_item_url')  # Redirect to the same page to display the form again
-        return render(request, 'remove_item.html', {'form': form})
-
-
-class EditItemStatusView(View):
-    def get(self, request):
-        form = EditItemStatusForm()
-        return render(request, 'edit_item_status.html', {'form': form})
-
-    def post(self, request):
-        form = EditItemStatusForm(request.POST)
-        if form.is_valid():
-            item_id = form.cleaned_data['item_id']
-            new_status = form.cleaned_data['new_status']
-            item = Item.objects.get(id=item_id)
-            item.item_status = new_status
-            item.save()
-            messages.success(request, "وضعیت محصول با موفقیت ویرایش شد") 
-            return redirect('edit_item_status_url')  # Redirect to the same page to display the form again
-        return render(request, 'edit_item_status.html', {'form': form})
-
+        
 class StffProfileView(LoginRequiredMixin,View):
      def get(slef,request):
           return render(request,'accounts/profile.html')
@@ -194,8 +124,7 @@ class StaffCategoryUpdateView(LoginRequiredMixin, View):
             messages.success(request, 'آپدیت اطلاعات شخصی با موفقیت انجام شد', 'success')
             return redirect('accounts:staff-categories')
         
-        return render(request, self.template_name, {'form': form})
-    
+        return render(request, self.template_name, {'form': form})   
 class StaffAddCategoryView(LoginRequiredMixin, View):
     form_class = CategoryForm
     template_name = "accounts/profile-add-category.html"
@@ -218,9 +147,34 @@ class StaffAddCategoryView(LoginRequiredMixin, View):
             return redirect('accounts:staff-categories')
         
         return render(request, self.template_name, {'form': form})
-    
-
-
+class StaffProfileItemsView(LoginRequiredMixin,View):
+    def get(self,request):
+        items = Item.objects.all()
+        return render(request,'accounts/items.html',{'items':items})
+class StaffProfileDeleteItemView(LoginRequiredMixin,View):
+     def get(self,request,id_item):
+          item = get_object_or_404(Item, pk=id_item)
+          item.delete()
+          messages.success(request,"محصول با موفقیت حذف شد","success")
+          return redirect("accounts:staff-items")
+class StaffProfileUpdateView(LoginRequiredMixin,View):
+     form_class = ItemForm
+     template_name = "accounts/profile-add-item.html"
+     def get(self,request,id_item) :
+        item = get_object_or_404(Item, id=id_item)
+        form = self.form_class(instance=item)
+        return render(request, self.template_name, {'form': form})
+     
+     def post(self,request,id_item):
+        item = get_object_or_404(Item, id=id_item)
+        form = self.form_class(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'آپدیت محصول با موفقیت انجام شد', 'success')
+            return redirect('accounts:staff-items')
+        else:
+            form = self.form_class(instance=item)
+            return render(request, self.template_name, {'form': form})  
 class StatisticsView(TemplateView):
     template_name = 'statistics.html' # Here, you can enter the template name you want to show the statestics
 
