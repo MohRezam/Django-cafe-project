65147a3a247df38a0345bd7fed70d485aea13c32
MohammadMahdiGhasemi <ghasemi.ferdosi@gmail.com>
2024-01-07 13:03:37 +0330

    Added save-custom-cart-item/

--- cafe/urls.py
+++ cafe/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     # path("product_detail/<slug:slug>", product_detail, name="product_detail"),
     path("contact/", views.ContactUsView.as_view(), name="contact"),
     path("<slug:category_name>/", views.CafeMenuView.as_view(), name="cafe_menu"),
+    path('save-custom-cart-item/', views.CafeMenuView.as_view(), name='save_custom_cart_item'),
 ]
+
