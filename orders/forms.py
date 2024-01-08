4c5861095ea12a0e411a68443473566ea2e7f693
Mahdi-Sadeghi2 <mehditranslator1400@gmail.com>
2024-01-08 12:33:04 +0330

    Added docstring and comments

--- orders/forms.py
+++ orders/forms.py
@@ -2,11 +2,17 @@ from django import forms
 from .models import Order ,Discount
 
 class OrderForm(forms.ModelForm):
+    """
+    A form for creating or updating an Order object.
+    """
     class Meta:
         model = Order
         fields = ['description', 'table_number', 'customer_name', 'phone_number', 'discount_code']  
 
 class DiscountCodeForm(forms.ModelForm):
+    """
+    A form for creating or updating a Discount object.
+    """
     class Meta:
         model = Discount
         fields = ['code']
@@ -16,9 +22,15 @@ class DiscountCodeForm(forms.ModelForm):
 
 
 class UserSessionForm(forms.Form):
+    """
+    A form for capturing the user's phone number.
+    """
     phone_number=forms.CharField(max_length=11)
 
 class CartAddForm(forms.Form):
+    """
+    A form for adding items to the cart.
+    """
     quantity = forms.CharField()
     item_id=forms.CharField()
     action=forms.CharField()
