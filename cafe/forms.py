aba2ba2d0fb73fb9e14943d3906116a211a0290a
Mahdi-Sadeghi2 <mehditranslator1400@gmail.com>
2024-01-08 12:07:38 +0330

    Added docstring and comments

--- cafe/forms.py
+++ cafe/forms.py
@@ -2,10 +2,24 @@ from django import forms
 
 
 class CartAddForm(forms.Form):
+    """
+    A form for adding items to the cart.
+    
+    Fields:
+    - quantity: The quantity of the item to be added.
+    - item_id: The ID of the item to be added.
+    - action: The action to be performed (e.g., add, remove, update).
+    """
     quantity = forms.CharField(initial=0)
     item_id=forms.CharField()
     action=forms.CharField()
     
 
 class SearchForm(forms.Form):
+    """
+    A form for searching products.
+    
+    Fields:
+    - search: The search query entered by the user.
+    """
     search = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"جست و جو محصولات"}))
