ab4a2c5f3d3f5b9acaae205a44f308869e7fa7d8
Mahdi-Sadeghi2 <mehditranslator1400@gmail.com>
2024-01-08 12:33:15 +0330

    Added docstring and comments

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Order
 # Register your models here.
-
+"""
+Customizes the admin interface for the Order model.
+"""
 
 admin.site.register(Order)
 # admin.site.register(OrderItem)
