3ad05790aabe6a5f92258f46730313f78171174b
MohRezam <mkalhor81126@gmail.com>
2024-01-10 01:35:49 +0330

    Register Table, Item, Category, Cafe and set list_display, search_fields & list_filter for admin panel

--- cafe/admin.py
+++ cafe/admin.py
@@ -1,10 +1,38 @@
 from django.contrib import admin
-from .models import Menu, Category, Item, Cafe
-# Register your models here.
+from .models import Menu, Category, Item, Cafe, Table
+
 """
 This file is responsible for registering models with the Django admin site.
 """
+
+# Register your models here.
+
+
 # admin.site.register(Menu)
-admin.site.register(Category) # Register the Category model with the admin site.
-admin.site.register(Item) # Register the Item model with the admin site.
-admin.site.register(Cafe)  # Register the Cafe model with the admin site.
+
+# @admin.register(Table)
+# class TableAdmin(admin.ModelAdmin):
+#     list_display = ('table_number', 'is_available')
+#     list_filter = ('is_available',)
+#     search_fields = ('table_number',)
+#     ordering = ('table_number',)
+admin.site.register(Table)
+
+@admin.register(Cafe)
+class CafeAdmin(admin.ModelAdmin):  # Register the Cafe model with the CafeAdmin.
+    list_display = ('name', 'phone_number')
+    search_fields = ('name', 'phone_number')
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):  # Register the Item model with the ItemAdmin.
+    list_display = ('name', 'price', 'category', 'item_status')
+    list_filter = ('category', 'item_status')
+    search_fields = ('name', 'category__category_name')
+    
+    
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name',)
+    search_fields = ('category_name',)
