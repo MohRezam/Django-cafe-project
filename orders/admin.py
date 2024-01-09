384683b11431fe16b5c0e9efbe3e57785e5fdc51
MohRezam <mkalhor81126@gmail.com>
2024-01-10 01:37:47 +0330

    Register Order and set list_display, list_filter & search_fields

--- orders/admin.py
+++ orders/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Order
-# Register your models here.
+
 """
 Customizes the admin interface for the Order model.
 """
 
-admin.site.register(Order)
-# admin.site.register(OrderItem)
+# Register your models here.
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_id', 'order_date', 'customer_name', 'table_number', 'order_status')
+    list_filter = ('order_status',)
+    search_fields = ('order_id', 'customer_name', 'table_number__table_number')
